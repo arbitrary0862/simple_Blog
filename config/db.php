6bf9f59a62f153def1cfdc6cebd21326b53bd1bf
Space <s23970207@gmail.com>
2024-09-29 21:39:40 +0800

    初始化專案

--- config/db.php
+++ config/db.php
@@ -1,7 +1,7 @@
 <?php
 // 資料庫設定
 define(constant_name: 'host',value: 'localhost');
-define(constant_name: 'db',value: '');
+define(constant_name: 'db',value: 'simple_blog');
 define(constant_name: 'user',value: '');
 define(constant_name: 'pass',value: '');
 define(constant_name: 'charset',value: 'utf8mb4');
